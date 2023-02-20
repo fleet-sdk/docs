a778ff47f49bd9933936a5624e69a9a57927388e
capt-nemo429 <capt-nemo429@users.noreply.github.com>
2023-02-20 19:34:36 +0000

    deploy: 0657661f8159996cffb22c7e79829d21253b208a

---
+++ assets/token-burning.md.15e54225.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as o}from"./app.ecc55692.js";const y=JSON.parse('{"title":"Burning Tokens","description":"","frontmatter":{},"headers":[],"relativePath":"token-burning.md","lastUpdated":1676920811000}'),e={name:"token-burning.md"},t=o("",6),p=[t];function l(c,r,i,u,d,A){return a(),n("div",null,p)}const C=s(e,[["render",l]]);export{y as __pageData,C as default};
